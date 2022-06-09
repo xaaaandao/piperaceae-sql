2cf437a62a4c55ac56d0befa1d6cf55af4bd6800
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-06-09 19:51:11 -0300

    Update dados_pr.sql

--- dados_pr.sql
+++ dados_pr.sql
@@ -22,14 +22,14 @@ SET GLOBAL local_infile = TRUE;
 ) 
 UNION
 SELECT
-	DISTINCT identified_by
-FROM d30m05a2022
-	JOIN cidades c on c.nome=d30m05a2022.state_province -- considera todas cidades do PR
+	*
+FROM dados
+	JOIN cidades c on c.nome=dados.state_province -- considera todas cidades do PR
 	WHERE
-	NOT (identified_by='' OR identified_by IS NULL) AND -- considera amostras identificadas
-	(country='Brazil' OR country='Brasil' OR country IS NULL) AND -- considera Brazil, Brasil e vazio
+	NOT (dados.identified_by='' OR dados.identified_by IS NULL) AND -- considera amostras identificadas
+	(dados.country='Brazil' OR dados.country='Brasil' OR dados.country IS NULL) AND -- considera Brazil, Brasil e vazio
 	(state_province LIKE 'Paran%' OR state_province='PR') AND -- considera Parana, Paraná e PR
-	NOT (county='' OR county IS NULL) -- evita cidade vazio
+	NOT (dados.county='' OR dados.county IS NULL) -- evita cidade vazio
     ORDER BY identified_by ASC
     INTO OUTFILE '/home/xandao/tmp/dados4.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'; -- exportar
     
