cc7ad2232856eea77c04e2aeb4b13e7d6d7fec66
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-06-09 20:59:40 -0300

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,4 +1,3 @@
 USE dados_herbarios;
 SET GLOBAL local_infile = TRUE;
-LOAD DATA LOCAL INFILE '/home/xandao/herbario/dados.csv' INTO TABLE dados
-		FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE '/home/xandao/herbario/dados.csv' INTO TABLE dados FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
