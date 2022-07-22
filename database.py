c1d869a4edaa60b60e6a5c09a94372745d5180a8
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-07-22 19:05:58 -0300

    Add atualização em largura

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import sqlalchemy.orm
 
 
 def connect(cfg):
-    list_hosts = ["localhost", "192.168.0.160"]
+    list_hosts = ("192.168.0.160", "localhost")
     for host in list_hosts:
         try:
             engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{host}:{cfg['port']}/{cfg['database']}", echo=True, pool_pre_ping=True)
