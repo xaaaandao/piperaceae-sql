182df9dee7153e01c04676875bc3d92ba0e4ea39
Alexandre Kajihara <alexandre.ykz@gmail.com>
2022-07-25 19:21:00 +0000

    Arrumando o inupdate

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ import sqlalchemy.orm
 
 
 def connect(cfg):
-    list_hosts = ("192.168.0.160", "localhost")
+    list_hosts = list(["localhost", "192.168.0.160"])
     for host in list_hosts:
         try:
-            engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{host}:{cfg['port']}/{cfg['database']}", pool_pre_ping=True)
+            engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{host}:{cfg['port']}/{cfg['database']}", echo=True, pool_pre_ping=True)
             Session = sqlalchemy.orm.sessionmaker(bind=engine)
             Session.configure(bind=engine)
             session = Session()
