5fce9cc77c45a0117460335d90204c7a198eeda5
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-07-27 08:12:16 -0300

    Change host db

--- database.py
+++ database.py
@@ -5,17 +5,17 @@ import sqlalchemy.schema
 
 
 def connect(cfg):
-    list_hosts = list(["192.168.0.160"])
-    for host in list_hosts:
-        try:
-            engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{host}:{cfg['port']}/{cfg['database']}", echo=True, pool_pre_ping=True)
-            Session = sqlalchemy.orm.sessionmaker(bind=engine)
-            Session.configure(bind=engine)
-            session = Session()
-            if engine.connect():
-                return engine, session
-        except Exception as e:
-            print(f"problems with host {host} ({e})")
+    try:
+        engine = sqlalchemy.create_engine(
+            f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{cfg['host']}:{cfg['port']}/{cfg['database']}", echo=True,
+            pool_pre_ping=True)
+        Session = sqlalchemy.orm.sessionmaker(bind=engine)
+        Session.configure(bind=engine)
+        session = Session()
+        if engine.connect():
+            return engine, session
+    except Exception as e:
+        print(f"problems with host {cfg['host']} ({e})")
 
 
 def make_operation(session):
--- dataframe.py
+++ dataframe.py
@@ -6,7 +6,8 @@ from tables import DataSP
 
 
 def convert_header_to_snake_case(dataframe):
-    return {column_name: re.sub(r'(?<!^)(?=[A-Z])', '_', column_name).lower() for column_name in get_columns_dataframe(dataframe)}
+    return {column_name: re.sub(r'(?<!^)(?=[A-Z])', '_', column_name).lower() for column_name in
+            get_columns_dataframe(dataframe)}
 
 
 def change_header(dataframe):
@@ -14,12 +15,18 @@ def change_header(dataframe):
 
 
 def get_columns_numeric(dataframe, table):
-    list_of_columns_numeric = list([])
-    for columns_dataframe in get_columns_dataframe(dataframe):
-        for columns_table in get_columns_table(table):
-            if check_if_column_is_numeric(columns_dataframe, columns_table):
-                list_of_columns_numeric.append(columns_dataframe)
-    return list_of_columns_numeric
+    list_columns_dataframe = list([column.lower() for column in get_columns_dataframe(dataframe)])
+    list_columns_table = list([column for column in get_columns_table(table)])
+    return list([c_table.key for c_table in list_columns_table if
+                 exits_column_table_in_dataframe(c_table, list_columns_dataframe) and column_table_is_numeric(c_table)])
+
+
+def column_table_is_numeric(c_table):
+    return "int" in str(c_table.type).lower() or "float" in str(c_table.type).lower()
+
+
+def exits_column_table_in_dataframe(c_table, list_columns_dataframe):
+    return c_table.key in list_columns_dataframe
 
 
 def get_columns_table(table):
@@ -31,7 +38,7 @@ def get_columns_dataframe(dataframe):
 
 
 def check_if_column_is_numeric(columns_dataframe, columns_table):
-    return str(columns_dataframe) in str(columns_table) and str(columns_table.type).lower() in ("int", "float")
+    return str(columns_dataframe) in str(columns_table.type) and str(columns_table.type).lower() in ("int", "float")
 
 
 def replace_nan_to_null(dataframe):
