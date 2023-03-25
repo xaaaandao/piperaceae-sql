ffe552c5dd71c87b25ba0ac5ea9a3b7b8e663110
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-03-25 15:43:19 -0300

    update files

--- images.py
+++ images.py
@@ -1,4 +1,3 @@
-import database
 import database as db
 import os
 import pandas as pd
@@ -6,14 +5,19 @@ import shutil
 
 from IPython.display import display
 
+
 def get_list_of_images_invalid():
     return {
-        'barcode': ['INPA0248526', 'INPA0248523', 'INPA0248528', 'NY01421575_01', 'HUFSJ001689_v00', 'HUFSJ001133_v00', 'HUFSJ002198_v00', 'HUFSJ003255_v00', 'HVASF000487_v01', 'INPA0019084_nd', 'INPA0022379_nd', 'INPA0032742_nd', 'INPA0023115', 'NL-U1484137', 'INPA0012286', 'INPA0146998'],
-        'reason': ['horizontal', 'horizontal', 'horizontal', 'horizontal', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'label', 'label', 'label', 'letter', 'letter', 'letter', 'incomplete']
+        'barcode': ['INPA0248526', 'INPA0248523', 'INPA0248528', 'NY01421575_01', 'HUFSJ001689_v00', 'HUFSJ001133_v00',
+                    'HUFSJ002198_v00', 'HUFSJ003255_v00', 'HVASF000487_v01', 'INPA0019084_nd', 'INPA0022379_nd',
+                    'INPA0032742_nd', 'INPA0023115', 'NL-U1484137', 'INPA0012286', 'INPA0146998'],
+        'reason': ['horizontal', 'horizontal', 'horizontal', 'horizontal', 'not exsicate', 'not exsicate',
+                   'not exsicate', 'not exsicate', 'not exsicate', 'label', 'label', 'label', 'letter', 'letter',
+                   'letter', 'incomplete']
     }
 
 
-#%%
+# %%
 def remove_images_invalid(list_level_name, list_images_invalid, list_path_images):
     list_path_correct = []
     list_count_path = []
@@ -50,7 +54,8 @@ def copy_images(list_level_name, list_path_images, path_out):
     return list_path_dst
 
 
-def save_metadata(color, image_size, list_level_name, list_path_images, list_count_path, list_path_dst, minimum_image, path_out, session):
+def save_metadata(color, image_size, list_level_name, list_path_images, list_count_path, list_path_dst, minimum_image,
+                  path_out, session):
     df = pd.DataFrame({
         'species': list_level_name,
         'paths': list_path_images,
@@ -65,7 +70,7 @@ def save_metadata(color, image_size, list_level_name, list_path_images, list_cou
     display(df.head(3))
     display('total of images %d' % df['count'].sum())
 
-    df = d.get_informations_images(list_path_images, session)
+    df = db.get_informations_images(list_path_images, session)
     filename_csv = os.path.join(path_out, 'image_informations.csv')
     df.to_csv(filename_csv, sep=';', na_rep=None, encoding='utf-8', lineterminator='\n', index=None)
     display('image informations')
@@ -73,15 +78,17 @@ def save_metadata(color, image_size, list_level_name, list_path_images, list_cou
     display(df.head(3))
 
 
-def separate_and_copy_images(condition, level, list_color, list_images_invalid, list_minimum_image, list_image_size, path_out, session):
+def separate_and_copy_images(condition, level, list_color, list_images_invalid, list_minimum_image, list_image_size,
+                             path_out, session):
     for color in list_color:
         for image_size in list_image_size:
             for minimum_image in list_minimum_image:
-                records = d.get_records_group_by_level(condition, level, minimum_image, session)
-                list_level_name, list_path_images = d.filter_records(color, image_size, minimum_image, records, session)
+                records = db.get_records_group_by_level(condition, level, minimum_image, session)
+                list_level_name, list_path_images = db.filter_records(color, image_size, minimum_image, records, session)
 
                 if len(list_path_images) > 0:
-                    list_count_path, list_path_images = remove_images_invalid(list_level_name, list_images_invalid, list_path_images)
+                    list_count_path, list_path_images = remove_images_invalid(list_level_name, list_images_invalid,
+                                                                              list_path_images)
 
                     path_out = os.path.join(path_out, color.upper(), image_size, str(minimum_image))
 
@@ -90,8 +97,10 @@ def separate_and_copy_images(condition, level, list_color, list_images_invalid, 
 
                     list_path_dst = copy_images(list_level_name, list_path_images, path_out)
 
-                    save_metadata(color, image_size, list_level_name, list_path_images, list_count_path, list_path_dst, minimum_image, path_out, session)
+                    save_metadata(color, image_size, list_level_name, list_path_images, list_count_path, list_path_dst,
+                                  minimum_image, path_out, session)
 
 
 def get_url_image(barcode, herbarium, height=5000, width=5000):
-    return 'https://specieslink.net/search/util/osd-dezoomify?imagecode=%s&path=herbaria/%s/%s&width=%s&height=%s' % (barcode, herbarium, barcode, str(width), str(height))
+    return 'https://specieslink.net/search/util/osd-dezoomify?imagecode=%s&path=herbaria/%s/%s&width=%s&height=%s' % (
+    barcode, herbarium, barcode, str(width), str(height))
