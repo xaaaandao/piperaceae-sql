18c8d21d4be59e13805d9fd8b56c29c54172322b
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-03-29 21:28:51 -0300

    update br_dataset.ipynb

--- images.py
+++ images.py
@@ -1,9 +1,10 @@
-import database as db
+import itertools
 import os
 import pandas as pd
 import shutil
 
 from IPython.display import display
+from models import DataTrustedIdentifier
 
 
 def get_list_of_images_invalid():
@@ -13,87 +14,41 @@ def get_list_of_images_invalid():
     }
 
 
-# %%
-def remove_images_invalid(list_level_name, list_images_invalid, list_path_images):
-    list_path_correct = []
-    list_count_path = []
-    for i, p in enumerate(list_path_images):
-        matching = [path for path in p if not any(barcode in path for barcode in list_images_invalid['barcode'])]
-
-        if len(p) != len(matching):
-            print('specie: %s before: %d after: %d' % (list_level_name[i], len(p), len(matching)))
-            diff = list(set(p) ^ set(matching))
-            print('diff: %s' % str(diff))
-
-        list_path_correct.append(matching)
-        list_count_path.append(len(matching))
-
-    return list_count_path, list_path_correct
-
-
-def copy_images(list_level_name, list_path_images, path_out):
-    list_path_dst = []
-
-    for i, species_and_paths in enumerate(zip(list_level_name, list_path_images), start=1):
-        species = species_and_paths[0]
-        list_p = species_and_paths[1]
-        path_final = os.path.join(path_out, 'f%d' % i)
-
-        if not os.path.exists(path_final):
-            os.makedirs(path_final)
-
-        # for p in list_p:
-        #     shutil.copy(p, path_final)
-
-        list_path_dst.append(path_final)
-
-    return list_path_dst
-
-
-def save_metadata(color, image_size, list_level_name, list_path_images, list_count_path, list_path_dst, minimum_image,
-                  path_out, session):
+def save_metadata(list_count_samples, list_level, list_path_images_final, list_seq_final, session, out):
     df = pd.DataFrame({
-        'species': list_level_name,
-        'paths': list_path_images,
-        'count': list_count_path,
-        'dst': list_path_dst
+        'levels': list_level,
+        'paths': list_path_images_final,
+        'count': list_count_samples,
+        'seq': list_seq_final,
     })
-    display('color: %s image_size: %s minimum_image: %d' % (color, image_size, minimum_image))
-    display('len(list_level_name): %d len(paths_images): %d' % (len(list_level_name), df['count'].sum()))
-    filename_csv = os.path.join(path_out, 'dataset_informations.csv')
-    display('save csv in %s' % filename_csv)
-    df.to_csv(filename_csv, sep=';', na_rep=None, encoding='utf-8', lineterminator='\n', index=None)
-    display(df.head(3))
-    display('total of images: %d' % df['count'].sum())
+    print('total of images: %d' % df['count'].sum())
+    display(df.head(4))
+    filename=os.path.join(out, 'info_dataset.csv')
+    df.to_csv(filename, sep=';', index=None, lineterminator='\n', doublequote=True)
 
-    df = db.get_informations_images(color, image_size, list_path_images, session)
-    filename_csv = os.path.join(path_out, 'image_informations.csv')
-    df.to_csv(filename_csv, sep=';', na_rep=None, encoding='utf-8', lineterminator='\n', index=None)
-    display('image informations')
-    display('save csv in %s' % filename_csv)
-    display(df.head(3))
-
-
-def separate_and_copy_images(condition, level, list_color, list_images_invalid, list_minimum_image, list_image_size, path_out, session):
-    for color in list_color:
-        for image_size in list_image_size:
-            for minimum_image in list_minimum_image:
-                records = db.get_records_group_by_level(condition, level, minimum_image, session)
-                list_level_name, list_path_images = db.filter_records(color, (int(image_size), int(image_size)), minimum_image, records, session)
-
-                if len(list_path_images) > 0:
-                    list_count_path, list_path_images = remove_images_invalid(list_level_name, list_images_invalid, list_path_images)
-
-                    path_dst = os.path.join(path_out, color.upper(), image_size, str(minimum_image))
-
-                    if not os.path.exists(path_dst):
-                        os.makedirs(path_dst)
-
-                    list_path_dst = copy_images(list_level_name, list_path_images, path_dst)
-
-                    save_metadata(color, (int(image_size), int(image_size)), list_level_name, list_path_images, list_count_path, list_path_dst, minimum_image, path_dst, session)
+    list_seq = list(itertools.chain(*list_seq_final))
+    query = session.query(DataTrustedIdentifier)\
+                    .filter(DataTrustedIdentifier.seq.in_(list_seq))\
+                    .all()
+    data = [(q.seq, q.genus, q.specific_epithet, q.genus_trusted, q.specific_epithet, q.list_src) for q in query]
+    columns = ['seq', 'genus', 'specific_epithet', 'genus_trusted', 'specific_epithet_trusted', 'urls']
+    df = pd.DataFrame(data, columns=columns)
+    display(df.head(4))
+    filename=os.path.join(out, 'info_samples.csv')
+    df.to_csv(filename, sep=';', index=None, lineterminator='\n', doublequote=True)
 
 
-def get_url_image(barcode, herbarium, height=5000, width=5000):
-    return 'https://specieslink.net/search/util/osd-dezoomify?imagecode=%s&path=herbaria/%s/%s&width=%s&height=%s' % (
-        barcode, herbarium, barcode, str(width), str(height))
+def copy_images(list_level, list_path_images_final, out):
+    if not os.path.exists(out):
+        os.makedirs(out)
+
+    for i, ff in enumerate(zip(list_level, list_path_images_final), start=1):
+        level = ff[0]
+        list_images = ff[1]
+
+        out_level = os.path.join(out, 'f%d' % i)
+        if not os.path.exists(out_level):
+            os.makedirs(out_level)
+
+        for i, image in enumerate(list_images, start=1):
+            shutil.copy(image, out_level)
