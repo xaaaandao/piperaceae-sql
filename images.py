273f3062d403aef53c59f9123d3f1b943f4e3cdf
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-04-03 00:56:08 -0300

    update files

--- images.py
+++ images.py
@@ -11,8 +11,8 @@ from models import DataTrustedIdentifier, Image
 
 def get_list_of_images_invalid():
     return {
-        'barcode': ['INPA0248526', 'INPA0248523', 'INPA0248528', 'NY01421926_01', 'NY01421575_01', 'HUFSJ001689_v00', 'HUFSJ001133_v00', 'HUFSJ002198_v00', 'HUFSJ003255_v00', 'HVASF000487_v01',  'HUFSJ001689_v01', 'INPA0019084_nd', 'INPA0022379_nd', 'INPA0032742_nd', 'INPA0023115', 'NL-U1484137', 'NY01397568_01', 'INPA0012286', 'INPA0146998'],
-        'reason': ['horizontal', 'horizontal', 'horizontal', 'horizontal', 'horizontal', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'label', 'label', 'label', 'letter', 'letter', 'letter', 'letter', 'incomplete']
+        'barcode': ['NY01421545_01', 'INPA0248526', 'INPA0248523', 'INPA0248528', 'NY01421926_01', 'NY01421575_01', 'HUFSJ001689_v00', 'HUFSJ001133_v00', 'HUFSJ002198_v00', 'HUFSJ003255_v00', 'HVASF000487_v01',  'HUFSJ001689_v01', 'INPA0019084_nd', 'INPA0022379_nd', 'INPA0032742_nd', 'INPA0023115', 'NL-U1484137', 'NY01397568_01', 'INPA0012286', 'INPA0146998'],
+        'reason': ['horizontal', 'horizontal', 'horizontal', 'horizontal', 'horizontal', 'horizontal', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'not exsicate', 'label', 'label', 'label', 'letter', 'letter', 'letter', 'letter', 'incomplete']
     }
 
 
@@ -108,5 +108,5 @@ def copy_images(list_level, list_path_images_final, out):
             os.makedirs(out_level)
 
         for i, image in enumerate(list_images, start=1):
-            shutil.copy(image, out_level)
+            shutil.copy(image.replace('w_pred_mask', 'jpeg'), out_level)
     return list_f
