0308cd8b21e8356f7e06d76f2df11022082d1e36
Alexandre Kajihara <alexandre.ykz@gmail.com>
2024-05-18 23:13:22 -0300

    up

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -27,38 +27,12 @@ class TestDatasets(TestCase):
                                     DatasetSamples.version.__eq__(1)))
 
         v2 = self.session.query(DatasetSamples) \
-            .filter(sa.and_(DatasetSamples.name.__eq__('br'),
+            .filter(sa.and_(DatasetSamples.name.__eq__('br_dataset'),
                             DatasetSamples.minimum.__eq__(5),
                             DatasetSamples.version.__eq__(2)))
 
         v1_seqs = [q.seq for q in v1]
         v2_seqs = [q.seq for q in v2]
 
-        # print(np.setdiff1d(v1_seqs, v2_seqs))
-
         diff = set(v1_seqs) - set(v2_seqs)
         print(len(diff), diff)
-
-        q = self.session.query(Exsiccata.seq, Level.genus, Level.specific_epithet)\
-            .join(Level, Exsiccata.level_id == Level.id) \
-            .filter(Exsiccata.seq.in_(diff))\
-            .order_by(Level.specific_epithet)\
-            .all()
-
-        for q2 in q:
-            print(q2.seq, q2.genus, q2.specific_epithet)
-
-        diff = set(v2_seqs) - set(v1_seqs)
-        print(len(diff))
-
-        # q = self.session.query(Exsiccata.seq, Level.genus_old, Level.specific_epithet_old, Level.genus, Level.specific_epithet) \
-        q = self.session.query(Exsiccata.seq, Local.country_old, Local.state_province_old, Local.county_old, Local.country, Local.state_province, Local.county) \
-            .join(Level, Exsiccata.level_id == Level.id) \
-            .join(Local, Exsiccata.local_id == Local.id) \
-            .filter(Exsiccata.seq.in_(diff)) \
-            .order_by(Level.specific_epithet) \
-            .all()
-
-        for q2 in q:
-            # print(q2.seq, q2.genus_old, q2.specific_epithet_old, q2.genus, q2.specific_epithet)
-            print(q2.seq, q2.country_old, q2.state_province_old, q2.county_old, q2.country, q2.state_province, q2.county)
